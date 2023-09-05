284fbb5ce56f39ca5052726348c1f2ba7c2c04ae
José María Soto valenzuela <116988804+Gorchon@users.noreply.github.com>
2023-09-04 22:31:59 -0600

    binary

--- hmk_1_3.cpp
+++ hmk_1_3.cpp
@@ -9,25 +9,21 @@
 #include <map> 
 #include <unordered_map>
 
-
-
 using namespace std;
 
-
-struct combined {
+struct MergedInfo {
     string original;
     int monthNum;
     int dayNum;
     int hourNum;
 };
 
-
 // this "||" is the or operator, so if the month number is the same, it will compare the day number, and if the day number is the same, it will compare the hour number if we use && instead of || it will only compare the month number and the day number, and if we use || instead of && it will only compare the month number and the hour number to compare the day number and the hour number we need to use && because we want to compare the day number and the hour number at the same time, if we use || it will only compare the day number and the hour number if the month number is the same, but we want to compare the day number and the hour number even if the month number is different, so we need to use && instead of ||
 // now we create a quicksort funtion that will sort the vector of structs by the month number including the day number and the hour number
 // to ensure that the hour number is included in the sorting we need to add a new variable to the struct that will hold the hour number, and we need to add a new condition to the if statement that will compare the hour number if the month number and the day number are the same, we can do it like this 
 // if (a[j].monthNum < pivot || (a[j].monthNum == pivot && a[j].dayNum <= a[r].dayNum) || (a[j].monthNum == pivot && a[j].dayNum == a[r].dayNum && a[j].hourNum <= a[r].hourNum)) {
 
-void quicksort(vector<combined>& a, int l, int r) {
+void quicksort(vector<MergedInfo>& a, int l, int r) {
     if (l < r) {
         int pivotMonth = a[r].monthNum;
         int pivotDay = a[r].dayNum;
@@ -49,7 +45,24 @@ void quicksort(vector<combined>& a, int l, int r) {
     }
 }
 
-
+int binarySearch(const vector<MergedInfo>& a, int l, int r, int targetMonth, int targetDay) {
+    while (l <= r) {
+        int mid = l + (r - l) / 2; // Calculate the middle index.
+        
+        // Compare the middle element with the target.
+        if (a[mid].monthNum == targetMonth && a[mid].dayNum == targetDay) {
+            return mid; // Found a match.
+        } else if (a[mid].monthNum < targetMonth || (a[mid].monthNum == targetMonth && a[mid].dayNum < targetDay)) {
+            // If the target is greater than the middle element, search in the right half.
+            l = mid + 1;
+        } else {
+            // Otherwise, search in the left half.
+            r = mid - 1;
+        }
+    }
+    
+    return -1; // Element not found.
+}
 
 
     int main() {
@@ -80,7 +93,7 @@ void quicksort(vector<combined>& a, int l, int r) {
         }
         inputfile.close(); // we close the file
 
-        vector<combined> sorted_data; // we create a vector of structs called data2
+        vector<MergedInfo> sorted_data; // we create a vector of structs called data2
 
         for (int i = 0; i < data.size(); i++){
             string original_line = data[i];  // we create a string called original_line to store the original line of the file
@@ -93,7 +106,7 @@ void quicksort(vector<combined>& a, int l, int r) {
             //we want to get the numbers without the ":" so we need to use atoi to convert the string to an int and we need to use c_str() to convert the string to a char, we need to ignore the ":" because we want to compare the numbers and if we don't ignore the ":" it will compare the numbers with the ":" and it will not work
             //no we get the numbers that we want ignoring the ":" and we store them in an int called hour_num, to do this we need to multiply the hours 60*60 times and the minutes 60 times and then we add them together
             int hour_num = atoi(hour.substr(0, 2).c_str()) * 60 * 60 + atoi(hour.substr(3, 2).c_str()) * 60 + atoi(hour.substr(6, 2).c_str());
-            combined c; // we create a struct called c
+            MergedInfo c; // we create a struct called c
             c.original = original_line; // we store the original line in the struct
             c.monthNum = month_num; // we store the number of the month in the struct
             c.dayNum = day_num; // we store the number of the day in the struct
@@ -109,6 +122,23 @@ void quicksort(vector<combined>& a, int l, int r) {
         }
         outputfile.close(); // we close it in order to save the changes
 
+        int targetMonth, targetDay;
+        cout << "Enter the month (1-12): ";
+        cin >> targetMonth;
+        cout << "Enter the day: ";
+        cin >> targetDay;
+
+        // Perform binary search to find the entries.
+        int resultIndex = binarySearch(sorted_data, 0, sorted_data.size() - 1, targetMonth, targetDay);
+        //to cout all the results that match the target we need to use a while loop, but with and if to check if the result is -1, if it is -1 it means that there are no results that match the target
+        if (resultIndex == -1) {
+            cout << "I cant´t find what you are searching" << endl;
+        } else {
+            while (resultIndex >= 0 && sorted_data[resultIndex].monthNum == targetMonth && sorted_data[resultIndex].dayNum == targetDay) {
+                cout << sorted_data[resultIndex].original << endl;
+                resultIndex--;
+            }
+        }
         return 0;
 
     }
