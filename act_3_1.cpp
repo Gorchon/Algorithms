66829a6e10a8acf0a4d18a849bf6e738f7a4be05
Gorchon <A01254831@tec.mx>
2023-10-07 01:04:36 -0600

    3.1_linux

--- act_3_1.cpp
+++ act_3_1.cpp
@@ -1,6 +1,9 @@
 #include <iostream> 
 #include <string>
 #include <vector>
+#include <queue>
+#include <stack>
+
 
 using namespace std;
 
@@ -29,7 +32,7 @@ class Binary_search_tree {
         Binary_search_tree():Root(NULL){}; // constructor of the class Binary_search_tree, we do it in order to initialize the values of the class
         ~Binary_search_tree(){
             DeleteBST(Root); // we call the function DeleteBST to delete the tree
-            cout  << "\nThe tree has been deleted\n" << endl; 
+            cout  << "\nDestructor: BST deleted\n" << endl; 
         }; 
 
         
@@ -48,49 +51,157 @@ class Binary_search_tree {
 
 //now we define Insert 
 void Binary_search_tree::Insert(int &value, Node *&Root) {
-    if (Root == NULL) { // if the root is null then we create a new node and we assign the value to the data of the node
+    if (Root == NULL) { // if the root is null then we create a new node and we assign the value to the data of the node, null because 
         Root = new Node(value);
     } else if (value < Root->data) { // if the value is less than the data of the root then we call the function Insert and we pass the value and the left of the root
         Insert(value, Root->left);
     } else if (value > Root->data) { // if the value is greater than the data of the root then we call the function Insert and we pass the value and the right of the root
         Insert(value, Root->right);
     } else { // if the value is equal to the data of the root then we print that the value is already in the tree
-        cout << "The value " << value << " is already in the tree and we cannot have repeated values" << endl; 
+        cout << "The value " << value << " is already in the tree" << endl;
     }
 }
- 
 
-void Binary_search_tree::PreOrder(Node *CurrentData) {
-    if (CurrentData == NULL) { // if the root is null then we return
+void Binary_search_tree::PreOrder(Node* Root) {
+    if (Root == NULL) { // if the root is null then we return
         return;
     } else {
-        cout << CurrentData->data << " "; // we print the data of the root
-        PreOrder(CurrentData->left); // we call the function PreOrder and we pass the left of the root
-        PreOrder(CurrentData->right); // we call the function PreOrder and we pass the right of the root
+        cout << Root->data << " "; // we print the data of the root
+        PreOrder(Root->left); // we call the function PreOrder and we pass the left of the root
+        PreOrder(Root->right); // we call the function PreOrder and we pass the right of the root
     }
 }
 
-void Binary_search_tree::InOrder(Node *CurrentData) {
-       if (CurrentData == NULL) { // if the root is null then we return
+void Binary_search_tree::InOrder(Node* Root) {
+    if (Root == NULL) { // if the root is null then we return
         return;
-       } else {
-        InOrder(CurrentData ->left);
-        cout << CurrentData->data << "";
-        InOrder(CurrentData ->right); 
-       }
+    } else {
+        InOrder(Root->left); // we call the function InOrder and we pass the left of the root
+        cout << Root->data << " "; // we print the data of the root
+        InOrder(Root->right); // we call the function InOrder and we pass the right of the root
+    }
 }
 
-void Binary_search_tree::PostOrder(Node *CurrentData) {
-       if (CurrentData == NULL) { // if the root is null then we return
+void Binary_search_tree::PostOrder(Node* Root) {
+    if (Root == NULL) { // if the root is null then we return
         return;
-       } else {
-        PostOrder(CurrentData->left);
-        PostOrder(CurrentData ->right);
-        cout << CurrentData->data << ""; 
-       }
+    } else {
+        PostOrder(Root->left); // we call the function PostOrder and we pass the left of the root
+        PostOrder(Root->right); // we call the function PostOrder and we pass the right of the root
+        cout << Root->data << " "; // we print the data of the root
+    }
 }
+
+void Binary_search_tree::breadth_first_traversal() {
+    if (Root == NULL) { // if the root is null then we return
+        return;
+    } 
+    queue<Node*> Q; // we create a queue of nodes
+    Q.push(Root); 
+
+    Node *Aux; 
+    while(!Q.empty()){
+        Q.push(NULL); 
+
+        Aux = Q.front();
+        while(Aux != NULL){
+            cout << Aux->data << " ";
+
+            if(Aux->left != NULL){
+                Q.push(Aux->left); 
+            }
+            if(Aux->right != NULL){
+                Q.push(Aux->right); 
+            }
+            Q.pop();
+            Aux = Q.front();
+        }
+        Q.pop();
+        cout << endl;
+    }
+}
+
+void Binary_search_tree::substituteToMin(Node*& aptAux, Node*& aptNode) { //apt means pointer to a node
+    if (aptAux->left != NULL) { // if the left of the node is not null then we call the function substituteToMin and we pass the left of the node and the node
+        substituteToMin(aptAux->left, aptNode);
+    } else { // if the left of the node is null then we assign the data of the node to the data of the aptAux
+        aptNode->data = aptAux->data;
+        aptNode = aptAux;
+        aptAux = aptAux->right;
+    }
+
+}
+
+
+void Binary_search_tree::DeleteNode(int &value, Node*& Root) {
+    if (Root == NULL) { // if the root is null then we return
+        return;
+    } else if (value < Root->data) { // if the value is less than the data of the root then we call the function DeleteNode and we pass the value and the left of the root
+        DeleteNode(value, Root->left);
+    } else if (value > Root->data) { // if the value is greater than the data of the root then we call the function DeleteNode and we pass the value and the right of the root
+        DeleteNode(value, Root->right);
+    } else { // if the value is equal to the data of the root then we delete the node
+        Node *Aux = Root; // we create a node and we assign the root to the node
+        if (Root->left == NULL) { // if the left of the root is null then we assign the right of the root to the root
+            Root = Root->right;
+        } else if (Root->right == NULL) { // if the right of the root is null then we assign the left of the root to the root
+            Root = Root->left;
+        } else { // if the left and the right of the root are not null then we call the function substituteToMin and we pass the right of the root and the root
+            substituteToMin(Root->right, Aux);
+        }
+        cout << "The value " << value << " has been deleted" << endl;
+        delete Aux; // we delete the node
+    }
+}
+
+
+void Binary_search_tree::DeleteBST(Node*& Root) {
+    if (Root == NULL) { // if the root is null then we return
+        return;
+        //we make a post order traversal in order to delete the tree, we could do too a pre order traversal or an in order traversal
+    } else {
+        DeleteBST(Root->left); // we call the function DeleteBST and we pass the left of the root
+        DeleteBST(Root->right); // we call the function DeleteBST and we pass the right of the root
+        delete Root; // we delete the root
+   }
+  
+}
+
+
 
 
 int main(){
+  
+
+    Binary_search_tree BST; // we create a binary search tree
+
+    vector<int> Vector = {47, 60, 22, 12, 6, 13, 41, 20, 52, 16}; 
+
+    for(int i : Vector){
+        BST.Insert(i);
+    }
+
+    cout << "In order: ";
+    BST.InOrder();
+    cout << endl;
+    cout << "Pre order: ";
+    BST.PreOrder();
+    cout << endl;
+    cout << "Post order: ";
+    BST.PostOrder();
+    cout << endl;
+    cout << "Breadth first traversal: " << endl;
+    BST.breadth_first_traversal();
+    cout << endl;
+
+    int node = 16;
+    BST.DeleteNode(node);
+    BST.breadth_first_traversal();
+    cout << endl;
+
+    node = 13; 
+    BST.DeleteNode(node);
+    BST.breadth_first_traversal();
+
     return 0; 
-}; 
+}
